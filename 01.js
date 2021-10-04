d3f3f9768cddc955f5f6ce3762c787f3f9f6e5b2
Guillaume Lodi <guillaume.lodi@gmail.com>
2021-10-04 14:22:17 +0200

    Ex01 - Ternay

---
+++ 01.js
@@ -0,0 +1,20 @@
+// Vous pouvez vérifier que votre code fonctionne en le lançant manuellement,
+// ou avec la commande `yarn test` ou `npm run test`.
+
+// Certains clients veulent un kebab sans gluten. D'autres avec.
+// La fonction ci-dessous doit retourner 'sans-gluten' ou 'avec-gluten' en fonction
+// du choiz du client.
+// 💡 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+function glutenOrNotGluten(withGluten) {
+
+}
+
+/**
+ * @param {string} kebab Une chaine de caractere representant le kebab i.e salade galette tomate oignons
+ * @param {boolean} withGluten boolean
+ */
+function makeKebab(kebab, withGluten) {
+    
+}
+
+module.exports = { glutenOrNotGluten, makeKebab }
