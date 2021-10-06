ce1d6d5f3fa90d04f6c53c98660d9ad1f756f4d1
Guillaume Lodi <guillaume.lodi@gmail.com>
2021-10-06 23:58:03 +0200

    Quick style

--- burgerstore/src/App.js
+++ burgerstore/src/App.js
@@ -9,7 +9,14 @@ function App() {
   // TODO with hooks
   return (
     <div className="App">
-      <nav>Le bon burger</nav>
+      <nav>
+        <img
+          height="100px"
+          src="https://cdn.dribbble.com/users/1787323/screenshots/9382093/media/db80fee4b04ff0cdcc3d51dee78a5275.png"
+          alt="Logo le bon burger"
+        />
+        Le bon burger
+      </nav>
       <div className="container">
         {/* TODO: Use shortcut props for burger */}
         {burgers.map(burger => (
