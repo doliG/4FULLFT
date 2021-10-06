a54c59988fa76d4f7b23ae1fe52fc9393eddb7ca
Guillaume Lodi <guillaume.lodi@gmail.com>
2021-10-06 16:13:05 +0200

    Ex04 solutions

--- rappels_js/04.js
+++ rappels_js/04.js
@@ -2,37 +2,30 @@
 // Retournez simplement les deux premiers kebabs.
 // 💡 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 
-function serveKebabs1And3(kebabs) {
-    // ...
-}
+const serveKebabs1And2 = ([k1, k2]) => [k1, k2]
 
 // Décidément, les clients affluent. Le chef vous demande d'être son commis de cuisine.
 // Vous allez devoir lui passer les objets au bon moment. Voici une représentation de la cuisine:
 const cuisine = {
-    "toque": 1,
-    "sauces": ["andalouse", "blanche", "algérienne"],
-    "rotissoir": {
-        "heat": 180,
-        "remainingMeatWeight": 1200,
-    },
+  toque: 1,
+  sauces: ["andalouse", "blanche", "algérienne"],
+  rotissoir: {
+    heat: 180,
+    remainingMeatWeight: 1200,
+  },
 }
 
 // "Combien de viande reste-il sur le rotissoir 1 ?"
-const remainingMeatWeight = cuisine => {
-    // ...
+const remainingMeatWeight = ({ rotissoir }) => {
+  const { remainingMeatWeight } = rotissoir
+  return remainingMeatWeight
 }
 
 // Passe moi les sauces !
-const getSauces = cuisine => {
-  // ...
-}
+const getSauces = ({ sauces }) => sauces
 
 // Passe moi la sauce andalouse !
-const getSauceAndalouse = cuisine => {
- // ...
-}
+const getSauceAndalouse = ({ sauces }) => sauces[0]
 
 // Donne moi la toque et les sauces !
-const getToqueAndSauce = cuisine => {
-    // ...
-}
+const getToqueAndSauce = ({ toque, sauces }) => ({ toque, sauces })
